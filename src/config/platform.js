e8e4446880bcc33d57681009ca54914e3c930ed9
HaMoDyBr <78080184+hamodybr@users.noreply.github.com>
2026-08-26 01:54:00 +0300

    Add canonical RESTBR platform public config

---
+++ src/config/platform.js
@@ -0,0 +1,5 @@
+export const RESTBR_PLATFORM = Object.freeze({
+  supabaseUrl: 'https://xdqewaapwhmqlfotaofg.supabase.co',
+  publishableKey: 'sb_publishable_dOGkocLtn1WVvrxmu6TnJQ_8qyPyV-T',
+  requestTimeoutMs: 8000
+});
